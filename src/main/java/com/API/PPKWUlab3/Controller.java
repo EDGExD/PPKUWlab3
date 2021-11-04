b9f846001a3a59c0120c062ba98315bcd218305a
EDGExD <76109839+EDGExD@users.noreply.github.com>
2021-11-04 15:14:35 +0100

    Wykoannie zadania lab3

--- src/main/java/com/API/PPKWUlab3/Controller.java
+++ src/main/java/com/API/PPKWUlab3/Controller.java
@@ -8,20 +8,142 @@ import org.springframework.web.client.RestTemplate;
 @RestController
 public class Controller {
 
-    @GetMapping("/Lab3")
-    public String giveStringInfo(@RequestParam String str){
+    @GetMapping("/JSON")
+    public String makeJSON(@RequestParam String str){
         String url = "http://localhost:8080/API?str=" +str;
 
-        System.out.println(str);
         RestTemplate restTemplate = new RestTemplate();
         String result = restTemplate.getForObject(url, String.class);
 
-        System.out.println(result);
-        return result;
+        return giveJSON(result);
     }
 
-//    @GetMapping("/")
-//    public String help(){
-//        return "Aby skorzystać z zaimplementowanej funkcjonalności jako endpoint należy do URL dopisać - \"/API?str=\" a po znaku \"=\" dowolny tekst.";
-//    }
+    @GetMapping("/CSV")
+    public String makeCSV(@RequestParam String str){
+        String url = "http://localhost:8080/API?str=" +str;
+
+        RestTemplate restTemplate = new RestTemplate();
+        String result = restTemplate.getForObject(url, String.class);
+
+        return giveCSV(result);
+    }
+
+    @GetMapping("/XML")
+    public String makeXML(@RequestParam String str){
+        String url = "http://localhost:8080/API?str=" +str;
+
+        RestTemplate restTemplate = new RestTemplate();
+        String result = restTemplate.getForObject(url, String.class);
+
+        return giveXML(result);
+    }
+
+    @GetMapping("/")
+    public String help(){
+        return "Aby skorzystać z zaimplementowanej funkcjonalności jako endpoint należy do URL dopisać - \"/Lab3?str=\" a po znaku \"=\" dowolny tekst.";
+    }
+
+    public String giveCSV (String str)
+    {
+        String substr = str;
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("Wielkie litery");
+        sb.append(',');
+        sb.append("Małe litery");
+        sb.append(',');
+        sb.append("Cyfry");
+        sb.append(',');
+        sb.append("Znaki specjalne");
+        sb.append(',');
+        sb.append("Suma znaków");
+        sb.append('\n');
+
+        for(int i =0; i <4; i++)
+        {
+            substr= substr.substring(substr.indexOf('>')+1);
+            sb.append(substr.substring(0,substr.indexOf(' ')));
+            sb.append(',');
+        }
+
+        substr= substr.substring(substr.indexOf('>')+1);
+        sb.append(substr.substring(0,substr.indexOf(' ')));
+        sb.append('\n');
+
+
+        return sb.toString();
+    }
+
+    public String giveJSON (String str)
+    {
+        String substr = str;
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("{");
+        sb.append("\"Wielkie litery\": ");
+
+        substr= substr.substring(substr.indexOf('>')+1);
+        sb.append(substr.substring(0,substr.indexOf(' ')));
+        sb.append(",\n");
+
+        sb.append("\"Małe litery\": ");
+        substr= substr.substring(substr.indexOf('>')+1);
+        sb.append(substr.substring(0,substr.indexOf(' ')));
+        sb.append(",\n");
+
+        sb.append("\"Cyfry\": ");
+        substr= substr.substring(substr.indexOf('>')+1);
+        sb.append(substr.substring(0,substr.indexOf(' ')));
+        sb.append(",\n");
+
+        sb.append("\"Znaki specjalne\": ");
+        substr= substr.substring(substr.indexOf('>')+1);
+        sb.append(substr.substring(0,substr.indexOf(' ')));
+        sb.append(",\n");
+
+        sb.append("\"Suma znaków\": ");
+        substr= substr.substring(substr.indexOf('>')+1);
+        sb.append(substr.substring(0,substr.indexOf(' ')));
+        sb.append('\n');
+        sb.append('}');
+
+        return sb.toString();
+    }
+
+    public String giveXML (String str)
+    {
+        String substr = str;
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n");
+        sb.append("<StringInfo>\n");
+
+        sb.append("<WielkieLitery>");
+        substr= substr.substring(substr.indexOf('>')+1);
+        sb.append(substr.substring(0,substr.indexOf(' ')));
+        sb.append("</WielkieLitery>\n");
+
+        sb.append("<MaleLitery>");
+        substr= substr.substring(substr.indexOf('>')+1);
+        sb.append(substr.substring(0,substr.indexOf(' ')));
+        sb.append("</MaleLitery>\n");
+
+        sb.append("<Cyfry>");
+        substr= substr.substring(substr.indexOf('>')+1);
+        sb.append(substr.substring(0,substr.indexOf(' ')));
+        sb.append("</Cyfry>\n");
+
+        sb.append("<ZnakiSpecjalne>");
+        substr= substr.substring(substr.indexOf('>')+1);
+        sb.append(substr.substring(0,substr.indexOf(' ')));
+        sb.append("</ZnakiSpecjalne>\n");
+
+        sb.append("<SumaZnaków>");
+        substr= substr.substring(substr.indexOf('>')+1);
+        sb.append(substr.substring(0,substr.indexOf(' ')));
+        sb.append("</SumaZnaków>\n");
+        sb.append("</StringInfo>\n");
+
+        return sb.toString();
+    }
 }
